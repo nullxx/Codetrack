a0d49c0eb90ce934334af25f91ac27ce69c402f4
Jon Lara <jon.laratrigo@gmail.com>
2020-10-27 11:37:46 +0100

    Create log folder if not exists

--- server/src/lib/logger.js
+++ server/src/lib/logger.js
@@ -1,7 +1,13 @@
+var fs = require('fs');
+const logPath = 'log';
+if (!fs.existsSync(logPath)) {
+    fs.mkdirSync(logPath);
+}
+
 const SimpleNodeLogger = require('simple-node-logger');
 
 const log = SimpleNodeLogger.createSimpleLogger({
-    logFilePath: 'log/all.log',
+    logFilePath: `${logPath}/all.log`,
     timestampFormat: 'YYYY-MM-DD HH:mm:ss.SSS'
 });
 
