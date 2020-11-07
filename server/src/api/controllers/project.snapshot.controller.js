b3f9724f53d933226eb06fac6d114003fd391987
Jon Lara <jon.laratrigo@gmail.com>
2020-11-07 17:36:39 +0100

    Fix check upload size for multiple uploads

--- server/src/api/controllers/project.snapshot.controller.js
+++ server/src/api/controllers/project.snapshot.controller.js
@@ -2,15 +2,30 @@ const loggerLib = require('../../lib/logger');
 
 const { createSnapshot, createMultipleSnapshots } = require('../../lib/project');
 
+
+const __checkSize = (data, req) => {
+    if (Array.isArray(data)) {
+        for (let i = 0; i < data.length; i++) {
+            const newData = data[i];
+            if (newData.size > parseInt(process.env.FILE_UPLOAD_MAX_BYTES)) {
+                throw new Error('ERR_SNAPSHOT_DATA_MAX_EXCEED');
+            }
+            loggerLib.log('info', '/project/snapshot - create', `File Bytes: ${newData.size}`, req);
+        }
+        return;
+    }
+    if (data.size > parseInt(process.env.FILE_UPLOAD_MAX_BYTES)) {
+        throw new Error('ERR_SNAPSHOT_DATA_MAX_EXCEED');
+    }
+    loggerLib.log('info', '/project/snapshot - create', `File Bytes: ${data.size}`, req);
+}
 const create = async (req, res, next) => {
     // TODO validate file input
     const { project, localPath } = req.body;
     const { data } = req.files;
     try {
-        loggerLib.log('info', '/project/snapshot - create', `File Bytes: ${data.size}`, req);
-        if (data.size > parseInt(process.env.FILE_UPLOAD_MAX_BYTES)) {
-            throw new Error('ERR_SNAPSHOT_DATA_MAX_EXCEED');
-        }
+        __checkSize(data, req);
+
         let createdSnapshot;
         if (Array.isArray(data)) {
             createdSnapshot = await createMultipleSnapshots({ fileDatas: data, project, localPath, user: req.user.id });
@@ -19,7 +34,6 @@ const create = async (req, res, next) => {
         }
         res.send({ code: 1, data: createdSnapshot });
         loggerLib.log('info', '/project/snapshot - create', `Created snapshot: ${JSON.stringify(createdSnapshot)}`, req);
-        loggerLib.log('info', '/project/snapshot - create', `Inserted file with bytes ${data.size} at ${JSON.stringify(createdSnapshot)}`, req);
     } catch (error) {
         loggerLib.log('error', '/project/snapshot - create', error, req);
         next(error);
