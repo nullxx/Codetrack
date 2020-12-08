3ed94a5b775ed4310625308a7139c54068c42db6
Jon Lara <jon.laratrigo@gmail.com>
2020-12-09 00:12:38 +0100

    Login add admin

--- server/src/api/controllers/auth.controller.js
+++ server/src/api/controllers/auth.controller.js
@@ -6,7 +6,8 @@ const { authUtils } = require('../../utils');
 const login = (req, res, next) => {
     try {
         let data = {
-            token: authUtils.sign(req.user, process.env.JWT_SECRET, process.env.JWT_EXPIRATION_TIME)
+            token: authUtils.sign(req.user, process.env.JWT_SECRET, process.env.JWT_EXPIRATION_TIME),
+            admin: req.user.role !== parseInt(process.env.USER_ROLE),
         };
         res.send({ code: 1, data });
         loggerLib.log('info', '/login - login', 'Logged', req);
