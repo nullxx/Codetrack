03775d5f337c88303e350c293512d87cd49d0993
Jon Lara <jon.laratrigo@gmail.com>
2020-12-27 16:32:53 +0100

    Added project natures

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const initialize = async () => {
 
     app.use(logger('dev'));
     app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
+    app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());
     app.use(express.static(path.join(__dirname, 'public')));
     app.use(fileUpload({
